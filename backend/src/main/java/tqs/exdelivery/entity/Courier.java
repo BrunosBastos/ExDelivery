8509d0956ec0320290d09281929b20f83da9bc88
mario <mariosilva@ua.pt>
2021-06-20 16:09:06 +0100

    removed code smells

--- backend/src/main/java/tqs/exdelivery/entity/Courier.java
+++ backend/src/main/java/tqs/exdelivery/entity/Courier.java
@@ -1,6 +1,5 @@
 package tqs.exdelivery.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
