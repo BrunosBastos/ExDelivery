2faa6726610d119ed6b3304d80c1625e00e1d3c4
mario <mariosilva@ua.pt>
2021-06-22 03:12:15 +0100

    changed config ports

--- projWebsite/src/config/index.ts
+++ projWebsite/src/config/index.ts
@@ -1,1 +1,9 @@
-export const EXDELIVERY_API_BASE_URL: string = 'http://localhost:8081/api/v1/';
+export let EXDELIVERY_API_BASE_URL: string = null;
+if (process.env.REACT_APP_RUNNING_MODE != null && process.env.REACT_APP_RUNNING_MODE == 'PRODUCTION'){
+    let HOST = "192.168.160.231"
+    EXDELIVERY_API_BASE_URL = `http://${HOST}:8081/api/v1/`;
+    console.log(EXDELIVERY_API_BASE_URL);
+}
+else {
+    EXDELIVERY_API_BASE_URL = 'http://localhost:8081/api/v1/';
+}
